1cc4c9a3c79a9068de6b086d66045f715622b0f2
xDedex <decellis.yeshua@gmail.com>
2019-10-22 22:53:12 -0200

    Inicio CRUD

--- ProjetoCRUD/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -5,10 +5,67 @@
  */
 package univs.edu.usuario;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author LABORATORIO 01
  */
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue
+    private int idusuario;
+    
+    @Column(length = 20, nullable = false)
+    private String login;
+    
+    @Column(length = 20, nullable = false)
+    private String Senha;
+
+    /**
+     * @return the idusuario
+     */
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    /**
+     * @param idusuario the idusuario to set
+     */
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the Senha
+     */
+    public String getSenha() {
+        return Senha;
+    }
+
+    /**
+     * @param Senha the Senha to set
+     */
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
     
 }
